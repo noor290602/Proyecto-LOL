2319fa3eb5de9e5fdb08f77c0a6f3f004aa1912b
Noor Aloune Sagouma <nalosag@gmail.com>
2024-08-19 16:32:30 +0200

    foreach de items.js mejorado

--- js/items.js
+++ js/items.js
@@ -10,18 +10,14 @@ fetch('https://ddragon.leagueoflegends.com/cdn/14.16.1/data/en_US/item.json') //
     itemsContainer.innerHTML = '';
     let htmlChampions = '';
 
-    let i = 0; //variable que me permite usar las "entries" del array "data"
-
-    Object.values(items).forEach(item => {
+    Object.entries(items).forEach(([key, item]) => {
         let urlImagenItem = `https://ddragon.leagueoflegends.com/cdn/14.16.1/img/item/${item.image.full}`;
 
-        htmlChampions += `<a href="itemProfile.html?id=${parseInt(Object.keys(json.data)[i])}">`; //forma de pasar variables por una URL -> ?id=
+        htmlChampions += `<a href="itemProfile.html?id=${key}">`; //forma de pasar variables por una URL -> ?id=
         htmlChampions += '<div class="tarjetaItem">';
         htmlChampions += '<div class="marcoImagenItems">';
         htmlChampions += `<div class="imagenItems" style="background-image: url(${urlImagenItem}); background-repeat: no-repeat;"></div></div>`;
         htmlChampions += '<p class="nombreItems">' + item.name + '</p></div></a>';    
-
-        i += 1;
     });
 
     itemsContainer.innerHTML = htmlChampions;
