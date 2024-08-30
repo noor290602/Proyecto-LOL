155fa6b11fc94430824a0deffff512396b9f4465
Noor Aloune Sagouma <nalosag@gmail.com>
2024-08-30 22:58:25 +0200

    Filtro roles champions.html

--- js/campeones.js
+++ js/campeones.js
@@ -1,61 +1,144 @@
-document.addEventListener('configLoaded', function() {
-    let championsSinFiltrar = [];
-    let textoBuscado = ""; //variable global
+document.addEventListener("configLoaded", function () {
+  let championsSinFiltrar = [];
+  let textoBuscado = ""; //variable global
 
-    fetch(`https://ddragon.leagueoflegends.com/cdn/${versionActual}/data/${idiomaActual}/champion.json`)
-        .then(response => response.json())
-        .then((response) => {
-            const json = response;
-            const champions = json.data;
-            championsSinFiltrar = Object.values(champions);
+  let roles = [];
+  let rolesSeleccionados = [];
+  let mostrarListaRoles = false;
 
-            //Buscador --------------------------------------------------------------------------------------------
-            let inputBuscador = document.getElementById("buscador"); //valor que escribe el user
+  fetch(
+    `https://ddragon.leagueoflegends.com/cdn/${versionActual}/data/${idiomaActual}/champion.json`
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      const json = response;
+      const champions = json.data;
+      championsSinFiltrar = Object.values(champions);
 
-            inputBuscador.addEventListener("input", () => {
-                textoBuscado = inputBuscador.value.toLowerCase();
-                mostrarCampeones();
-            })
+      //Roles --------------------------------------------------------------------------------------
 
-            //Borrar buscador --------------------------------------------------------------------------------------
-            let btnBorrar = document.getElementById("btnBorrar");
+      championsSinFiltrar.forEach((champion) => {
+        champion.tags.forEach((tag) => {
+          if (!roles.includes(tag)) {
+            roles.push(tag);
+          }
+        });
+      });
 
-            function limpiar() {
-                let inputBuscador = document.getElementById("buscador");
-                inputBuscador.value = ""; // Cambia innerContent por value
-                inputBuscador.dispatchEvent(new Event('input')); // Dispara el evento input
-            }
+      
 
-            btnBorrar.addEventListener("click", limpiar);
+      let flechaListaRoles = document.getElementById("flechaListaRoles");
+        flechaListaRoles.addEventListener("click", desplegarRoles);
 
-            mostrarCampeones();
-        })
+      //Buscador --------------------------------------------------------------------------------------------
+      let inputBuscador = document.getElementById("buscador"); //valor que escribe el user
 
-    function mostrarCampeones(){
-        let championsFiltrados = championsSinFiltrar;
+      inputBuscador.addEventListener("input", () => {
+        textoBuscado = inputBuscador.value.toLowerCase();
+        mostrarCampeones();
+      });
 
-        if (textoBuscado.length > 0){
-            championsFiltrados = championsFiltrados.filter((champion) => champion.name.toLowerCase().includes(textoBuscado));
-        }
+      //Borrar buscador --------------------------------------------------------------------------------------
+      let btnBorrar = document.getElementById("btnBorrar");
 
-        const championsContainer = document.getElementsByClassName('tarjetasChampions')[0];
-        championsContainer.innerHTML = '';
-        let htmlChampions = '';
+      function limpiar() {
+        let inputBuscador = document.getElementById("buscador");
+        inputBuscador.value = ""; // Cambia innerContent por value
+        inputBuscador.dispatchEvent(new Event("input")); // Dispara el evento input
+      }
 
-        if (championsFiltrados.length === 0){
-            championsContainer.innerHTML = `<p class="mensajeNoItems">NO HAY CAMPEONES QUE CUMPLAN ESTE REQUISITO</p>`;
-        } else {
-            championsFiltrados.forEach(champion => {
-                let urlImagenChampion = `https://ddragon.leagueoflegends.com/cdn/${versionActual}/img/champion/${champion.image.full}`;
+      btnBorrar.addEventListener("click", limpiar);
 
-                htmlChampions += `<a class="enlace" href="championsProfile.html?id=${champion.id}">`;
-                htmlChampions += '<div class="tarjetaCampeon">';
-                htmlChampions += '<div class="marcoImagenCampeon">';
-                htmlChampions += `<div class="imagenCampeon" style="background-image: url(${urlImagenChampion}); background-repeat: no-repeat;"></div></div>`;
-                htmlChampions += '<p class="nombreCampeon">' + champion.name + '</p></div></a>';    
-            });
+      mostrarCampeones();
+    });
 
-            championsContainer.innerHTML = htmlChampions;
-        }
+  function mostrarCampeones() {
+    let championsFiltrados = championsSinFiltrar;
+
+    if (textoBuscado.length > 0) {
+      championsFiltrados = championsFiltrados.filter((champion) =>
+        champion.name.toLowerCase().includes(textoBuscado)
+      );
     }
+
+    if (rolesSeleccionados.length > 0) {
+      championsFiltrados = championsFiltrados.filter(champion =>
+          rolesSeleccionados.every(rol =>
+              champion.tags.includes(rol)
+          )
+      );
+  }
+
+    const championsContainer = document.getElementsByClassName("tarjetasChampions")[0];
+    championsContainer.innerHTML = "";
+    let htmlChampions = "";
+
+    if (championsFiltrados.length === 0) {
+      championsContainer.innerHTML = `<p class="mensajeNoItems">NO HAY CAMPEONES QUE CUMPLAN ESTE REQUISITO</p>`;
+    } else {
+      championsFiltrados.forEach((champion) => {
+        let urlImagenChampion = `https://ddragon.leagueoflegends.com/cdn/${versionActual}/img/champion/${champion.image.full}`;
+
+        htmlChampions += `<a class="enlace" href="championsProfile.html?id=${champion.id}">`;
+        htmlChampions += '<div class="tarjetaCampeon">';
+        htmlChampions += '<div class="marcoImagenCampeon">';
+        htmlChampions += `<div class="imagenCampeon" style="background-image: url(${urlImagenChampion}); background-repeat: no-repeat;"></div></div>`;
+        htmlChampions +=
+          '<p class="nombreCampeon">' + champion.name + "</p></div></a>";
+      });
+
+      championsContainer.innerHTML = htmlChampions;
+    }
+  }
+
+  function desplegarRoles() {
+    mostrarListaRoles = !mostrarListaRoles;
+
+    let listaRolesChampions = document.getElementById("listaRolesChampions");
+
+    if (mostrarListaRoles) {
+      listaRolesChampions.style.display = "block"; //muestra la lista
+      document.getElementById("flechaListaRoles").style.transform = "scaleY(-1)"; //le damos la vuelta a la flecha
+      let contenidoRoles = "";
+
+      roles.forEach((rol) => {
+        const isChecked = rolesSeleccionados.includes(rol) ? "checked" : "";
+        contenidoRoles += `<li class="rolesChampions"><input type="checkbox" class="rolSeleccionado" value="${rol}" ${isChecked}/>${rol}</li>`;
+      });
+
+      listaRolesChampions.innerHTML = contenidoRoles;
+
+      //Filtrar
+      document.querySelectorAll(".rolSeleccionado").forEach((checkbox) => {
+        checkbox.addEventListener("change", (event) => {
+          let valorRol = checkbox.value; //por cada checkbox recogemos el valor
+          if (event.target.checked && !rolesSeleccionados.includes(valorRol)) {
+            //si está checkeado
+            rolesSeleccionados.push(valorRol); //lo añadimos al array de roles seleccionados
+          } else {
+            const indice = rolesSeleccionados.indexOf(valorRol);
+
+            if (indice > -1) { //si exite dentro del array
+              rolesSeleccionados.splice(indice, 1); //eliminamos el rol
+            }
+          }
+
+          //Mostramos las etiquetas clickadas abajo
+          let contenedorRolesMostrados = document.getElementById("rolesMostradosChampions");
+
+          let contenidoRolesMostrados = "";
+
+          rolesSeleccionados.forEach((et) => {
+            contenidoRolesMostrados += `<div class = "divRolesSeleccionados"><p>${et}</p></div>`;
+          });
+
+          contenedorRolesMostrados.innerHTML = contenidoRolesMostrados;
+          mostrarCampeones();
+        });
+      });
+    } else {
+        listaRolesChampions.style.display = "none"; //cierra la lista
+        document.getElementById("flechaListaRoles").style.transform = ""; //le damos la vuelta a la flecha
+    }
+  }
 });
--- js/items.js
+++ js/items.js
@@ -1,66 +1,139 @@
 document.addEventListener('configLoaded', function() {
-//Paso de variables a traves de localStorage
-let storage = window.localStorage;
-let version = storage.getItem("version");
-let idioma = storage.getItem("idioma");
-let itemsSinFiltrar = [];
-let textoBuscado = ""; //variable global
+    // Paso de variables a través de localStorage
+    let storage = window.localStorage;
+    let version = storage.getItem("version");
+    let idioma = storage.getItem("idioma");
+    let itemsSinFiltrar = [];
+    let textoBuscado = ""; // variable global
+    
+    let etiquetas = [];
+    let etiquetasSeleccionadas = [];
+    let mostrarListaEtiquetas = false;
 
-fetch(`https://ddragon.leagueoflegends.com/cdn/${version}/data/${idioma}/item.json`) 
-.then(response => response.json())
-.then((response) => {
-    const json = response;
-    const items = json.data;
-    itemsSinFiltrar = Object.entries(items);
+    fetch(`https://ddragon.leagueoflegends.com/cdn/${version}/data/${idioma}/item.json`)
+    .then(response => response.json())
+    .then((response) => {
+        const json = response;
+        const items = json.data;
+        itemsSinFiltrar = Object.entries(items);
 
-     //Buscador --------------------------------------------------------------------------------------
+        // Etiquetas --------------------------------------------------------------------------------------
+        itemsSinFiltrar.forEach(([key, item]) => {
+            item.tags.forEach(tag => {
+                if (!etiquetas.includes(tag)) {
+                    etiquetas.push(tag);
+                }
+            });
+        });
 
-     let inputBuscador = document.getElementById("buscador"); //valor que escribe el user
- 
-     inputBuscador.addEventListener("input", () => {
-        textoBuscado = inputBuscador.value.toLowerCase();
-        mostrarItems();
-     })
+        let flechaListaEtiquetas = document.getElementById("flechaListaEtiquetas");
+        flechaListaEtiquetas.addEventListener("click", desplegarEtiquetasItems);
 
-      //Borrar buscador --------------------------------------------------------------------------------------
-        let btnBorrar = document.getElementById("btnBorrar"); 
+        // Buscador --------------------------------------------------------------------------------------
+        let inputBuscador = document.getElementById("buscador"); // valor que escribe el user
+        inputBuscador.addEventListener("input", () => {
+            textoBuscado = inputBuscador.value.toLowerCase();
+            mostrarItems();
+        });
 
+        // Borrar buscador --------------------------------------------------------------------------------------
+        let btnBorrar = document.getElementById("btnBorrar");
         function limpiar() {
             let inputBuscador = document.getElementById("buscador");
             inputBuscador.value = ""; // Cambia innerContent por value
             inputBuscador.dispatchEvent(new Event('input')); // Dispara el evento input
         }
-
         btnBorrar.addEventListener("click", limpiar);
 
         mostrarItems();
-})
+    });
 
-function mostrarItems(){
-    let itemsFiltrados = itemsSinFiltrar;
+    function mostrarItems() {
+        let itemsFiltrados = itemsSinFiltrar;
 
-    if (textoBuscado.length > 0){
-        itemsFiltrados =  itemsFiltrados.filter(([key, item]) => item.name.toLowerCase().includes(textoBuscado));
+        if (textoBuscado.length > 0) {
+            itemsFiltrados = itemsFiltrados.filter(([key, item]) =>
+                item.name.toLowerCase().includes(textoBuscado)
+            );
+        }
+
+        if (etiquetasSeleccionadas.length > 0) {
+            itemsFiltrados = itemsFiltrados.filter(([key, item]) =>
+                etiquetasSeleccionadas.every(tag =>
+                    item.tags.includes(tag)
+                )
+            );
+        }
+
+        const itemsContainer = document.getElementById('tarjetasItems');
+        itemsContainer.innerHTML = '';
+        let htmlItems = '';
+
+        if (itemsFiltrados.length === 0) {
+            itemsContainer.innerHTML = `<p class="mensajeNoItems">NO HAY ITEMS QUE CUMPLAN ESTE REQUISITO</p>`;
+        } else {
+            itemsFiltrados.forEach(([key, item]) => {
+                let urlImagenItem = `https://ddragon.leagueoflegends.com/cdn/${version}/img/item/${item.image.full}`;
+                
+                htmlItems += `<a href="itemProfile.html?id=${key}">`; // forma de pasar variables por una URL -> ?id=
+                htmlItems += '<div class="tarjetaItem">';
+                htmlItems += '<div class="marcoImagenItems">';
+                htmlItems += `<div class="imagenItems" style="background-image: url(${urlImagenItem}); background-repeat: no-repeat;"></div></div>`;
+                htmlItems += '<p class="nombreItems">' + item.name + '</p></div></a>';
+            });
+        
+            itemsContainer.innerHTML = htmlItems;
+        }
     }
 
-    const itemsContainer = document.getElementById('tarjetasItems');
-    itemsContainer.innerHTML = '';
-    let htmlItems = '';
+    function desplegarEtiquetasItems() {
+        mostrarListaEtiquetas = !mostrarListaEtiquetas;
 
-    if (itemsFiltrados.length === 0){
-        itemsContainer.innerHTML = `<p class = "mensajeNoItems">NO HAY ITEMS QUE CUMPLAN ESTE REQUISITO</p>`;
-    }else {
-        itemsFiltrados.forEach(([key, item]) => {
-            let urlImagenItem = `https://ddragon.leagueoflegends.com/cdn/${version}/img/item/${item.image.full}`;
-            
-            htmlItems += `<a href="itemProfile.html?id=${key}">`; //forma de pasar variables por una URL -> ?id=
-            htmlItems += '<div class="tarjetaItem">';
-            htmlItems += '<div class="marcoImagenItems">';
-            htmlItems += `<div class="imagenItems" style="background-image: url(${urlImagenItem}); background-repeat: no-repeat;"></div></div>`;
-            htmlItems += '<p class="nombreItems">' + item.name + '</p></div></a>';        
-        });
-    
-        itemsContainer.innerHTML = htmlItems;
+        let listaEtiquetasItems = document.getElementById('listaEtiquetasItems');
+
+        if (mostrarListaEtiquetas) {
+            listaEtiquetasItems.style.display = "block"; // muestra la lista
+            document.getElementById('flechaListaEtiquetas').style.transform = "scaleY(-1)"; // le damos la vuelta a la flecha
+
+            let contenidoEtiqueta = "";
+
+            etiquetas.forEach(tag => {
+                const isChecked = etiquetasSeleccionadas.includes(tag) ? "checked" : "";
+                contenidoEtiqueta += `<li class="etiquetasItems"><input type="checkbox" class="etiquetaSeleccionada" value="${tag}" ${isChecked}/> ${tag}</li>`;
+            });
+
+            listaEtiquetasItems.innerHTML = contenidoEtiqueta;
+
+            // Filtrar
+            document.querySelectorAll(".etiquetaSeleccionada").forEach(checkbox => {
+                checkbox.addEventListener("change", (event) => {
+                    let valorEtiqueta = checkbox.value; // por cada checkbox recogemos el valor
+                    if (event.target.checked && !etiquetasSeleccionadas.includes(valorEtiqueta)) { // si está checkeado
+                        etiquetasSeleccionadas.push(valorEtiqueta); // lo añadimos al array de etiquetas seleccionadas
+                    } else {
+                        const indice = etiquetasSeleccionadas.indexOf(valorEtiqueta);
+                        
+                        if (indice > -1) { // si existe dentro del array
+                            etiquetasSeleccionadas.splice(indice, 1); // eliminamos la etiqueta
+                        }
+                    }
+
+                    // Mostramos las etiquetas seleccionadas abajo
+                    let contenedorEtiquetasMostradas = document.getElementById("etiquetasMostradasItems");
+                    let contenidoEtiquetasMostradas = "";
+
+                    etiquetasSeleccionadas.forEach(et => {
+                        contenidoEtiquetasMostradas += `<div class="divEtiquetasSeleccionadas"><p>${et}</p></div>`;
+                    });
+
+                    contenedorEtiquetasMostradas.innerHTML = contenidoEtiquetasMostradas;
+                    mostrarItems();
+                });
+            });
+
+        } else {
+            listaEtiquetasItems.style.display = "none"; // cierra la lista
+            document.getElementById('flechaListaEtiquetas').style.transform = ""; // le damos la vuelta a la flecha
+        }
     }
-}
 });
